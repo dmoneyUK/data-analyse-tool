c9bd0e22d2c458b7cef76d750610cd81d0339c59
Jinge Dai <jinge.dai@gamesys.co.uk>
2019-01-06 14:11:24 +0000

    Improved euromillion ticket parsing

--- src/main/java/my/lottery/services/LotterySyndicate/LotterySyndicateDataFetchingServiceImpl.java
+++ src/main/java/my/lottery/services/LotterySyndicate/LotterySyndicateDataFetchingServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static my.lottery.common.LotteryConstants.LOTTERY_SYNDICATE_URL;
 
@@ -28,102 +27,38 @@ public class LotterySyndicateDataFetchingServiceImpl implements DataFetchingServ
     public List<EuroMillionsTicketDto> fetchEuroMillionTickets() {
         String result = this.lotteryClient.get(LOTTERY_SYNDICATE_URL);
 
-        //List<EuroMillionsTicketDto> tickets =
-        //        Arrays.stream(result.split("<div class=\"fl-col-content fl-node-content\">"))
-        //              .filter(col -> col.contains("EuroMillions"))
-        //              .flatMap(content -> Arrays.stream(content.split("<p>Ticket number:</p>")))
-        //              .skip(1)
-        //              .flatMap(line -> Arrays.stream(line.split("<li>Lucky ")))
-        //              .skip(1)
-        //              .map(lucky -> {
-        //                  String[] uls = lucky.split("<ul>");
-        //                  EuroMillionsTicketDto euroMillionsTicket = new EuroMillionsTicketDto();
-        //                  Arrays.stream(uls)
-        //                        .map(ul -> ul.replace("\n", ""))
-        //                        .flatMap(ulInLine -> Arrays.stream(ulInLine.split("</ul>")))
-        //                        .forEach(li -> {
-        //                            if (li.matches("(<li>\\d\\d</li>){5}")) {
-        //                                String[] numbers = li.replace("<li>", "").split("</li>");
-        //                                euroMillionsTicket.setN1(Integer.valueOf(numbers[0]));
-        //                                euroMillionsTicket.setN2(Integer.valueOf(numbers[1]));
-        //                                euroMillionsTicket.setN3(Integer.valueOf(numbers[2]));
-        //                                euroMillionsTicket.setN4(Integer.valueOf(numbers[3]));
-        //                                euroMillionsTicket.setN5(Integer.valueOf(numbers[4]));
-        //                            } else if (li.matches("(<li>\\d\\d</li>){2}")) {
-        //                                String[] stars = li.replace("<li>", "").split("</li>");
-        //                                euroMillionsTicket.setS1(Integer.valueOf(stars[0]));
-        //                                euroMillionsTicket.setS2(Integer.valueOf(stars[1]));
-        //                            }
-        //                        });
-        //                  return euroMillionsTicket;
-        //              })
-        //              .collect(Collectors.toList());
-
         List<EuroMillionsTicketDto> tickets = new ArrayList<>();
         Arrays.stream(result.split("<div class=\"fl-col-content fl-node-content\">"))
               .filter(col -> col.contains("EuroMillions"))
-              .flatMap(content -> Arrays.stream(content.split("<p>Ticket number:</p>")))
-              .skip(1)
-              .flatMap(line -> Arrays.stream(line.split("<li>Lucky ")))
-              .skip(1)
-              .forEach(lucky -> {
-                  String[] uls = lucky.split("<ul>");
-                  EuroMillionsTicketDto euroMillionsTicket = new EuroMillionsTicketDto();
-                  Arrays.stream(uls)
-                        .map(ul -> ul.replace("\n", ""))
-                        .flatMap(ulInLine -> Arrays.stream(ulInLine.split("</ul>")))
-                        .forEach(li -> {
-                            if (li.matches("(<li>\\d\\d</li>){5}")) {
-                                String[] numbers = li.replace("<li>", "").split("</li>");
-                                euroMillionsTicket.setN1(Integer.valueOf(numbers[0]));
-                                euroMillionsTicket.setN2(Integer.valueOf(numbers[1]));
-                                euroMillionsTicket.setN3(Integer.valueOf(numbers[2]));
-                                euroMillionsTicket.setN4(Integer.valueOf(numbers[3]));
-                                euroMillionsTicket.setN5(Integer.valueOf(numbers[4]));
-                            } else if (li.matches("(<li>\\d\\d</li>){2}")) {
-                                String[] stars = li.replace("<li>", "").split("</li>");
-                                euroMillionsTicket.setS1(Integer.valueOf(stars[0]));
-                                euroMillionsTicket.setS2(Integer.valueOf(stars[1]));
-                                tickets.add(euroMillionsTicket);
-                            }else if (li.matches("(<li>\\S{4}\\d{5}</li>){4}")){
-                                String[] codes = li.replace("<li>", "").split("</li>");
-                                tickets.get(tickets.size()-4).setCode(codes[0]);
-                                tickets.get(tickets.size()-3).setCode(codes[1]);
-                                tickets.get(tickets.size()-2).setCode(codes[2]);
-                                tickets.get(tickets.size()-1).setCode(codes[3]);
-                            }
-                        });
+              .flatMap(content -> Arrays.stream(content.split("<ul>")))
+              .filter(ul -> ul.startsWith("\n<li>"))
+              .flatMap(ul -> Arrays.stream(ul.replace("\n", "").split("</ul>")))
+              .forEach(li -> {
+                  if (li.matches("(<li>\\d\\d</li>){5}")) {
+                      EuroMillionsTicketDto euroMillionsTicket = new EuroMillionsTicketDto();
+                      String[] numbers = li.replace("<li>", "").split("</li>");
+                      euroMillionsTicket.setN1(Integer.valueOf(numbers[0]));
+                      euroMillionsTicket.setN2(Integer.valueOf(numbers[1]));
+                      euroMillionsTicket.setN3(Integer.valueOf(numbers[2]));
+                      euroMillionsTicket.setN4(Integer.valueOf(numbers[3]));
+                      euroMillionsTicket.setN5(Integer.valueOf(numbers[4]));
+                      tickets.add(euroMillionsTicket);
+
+                  } else if (li.matches("(<li>\\d\\d</li>){2}")) {
+                      EuroMillionsTicketDto euroMillionsTicket = tickets.get(tickets.size() - 1);
+                      String[] stars = li.replace("<li>", "").split("</li>");
+                      euroMillionsTicket.setS1(Integer.valueOf(stars[0]));
+                      euroMillionsTicket.setS2(Integer.valueOf(stars[1]));
+
+                  } else if (li.matches("(<li>\\S{4}\\d{5}</li>){4}")) {
+                      String[] codes = li.replace("<li>", "").split("</li>");
+                      tickets.get(tickets.size() - 4).setCode(codes[0]);
+                      tickets.get(tickets.size() - 3).setCode(codes[1]);
+                      tickets.get(tickets.size() - 2).setCode(codes[2]);
+                      tickets.get(tickets.size() - 1).setCode(codes[3]);
+                  }
               });
 
-        //String[] lines = result.split("<p>Ticket number:</p>");
-        //
-        //List<EuroMillionsTicketDto> tickets = Arrays.stream(lines)
-        //                             .skip(1)
-        //                             .flatMap(line -> Arrays.stream(line.split("<li>Lucky ")))
-        //                             .skip(1)
-        //                             .map(lucky -> {
-        //                                 String[] uls = lucky.split("<ul>");
-        //                                 EuroMillionsTicketDto euroMillionsTicket = new EuroMillionsTicketDto();
-        //                                 Arrays.stream(uls)
-        //                                       .map(ul -> ul.replace("\n", ""))
-        //                                       .flatMap(ulInLine -> Arrays.stream(ulInLine.split("</ul>")))
-        //                                       .forEach(li -> {
-        //                                           if (li.matches("(<li>\\d\\d</li>){5}")) {
-        //                                               String[] numbers = li.replace("<li>", "").split("</li>");
-        //                                               euroMillionsTicket.setN1(Integer.valueOf(numbers[0]));
-        //                                               euroMillionsTicket.setN2(Integer.valueOf(numbers[1]));
-        //                                               euroMillionsTicket.setN3(Integer.valueOf(numbers[2]));
-        //                                               euroMillionsTicket.setN4(Integer.valueOf(numbers[3]));
-        //                                               euroMillionsTicket.setN5(Integer.valueOf(numbers[4]));
-        //                                           } else if (li.matches("(<li>\\d\\d</li>){2}")) {
-        //                                               String[] stars = li.replace("<li>", "").split("</li>");
-        //                                               euroMillionsTicket.setS1(Integer.valueOf(stars[0]));
-        //                                               euroMillionsTicket.setS2(Integer.valueOf(stars[1]));
-        //                                           }
-        //                                       });
-        //                                 return euroMillionsTicket;
-        //                             })
-        //                             .collect(Collectors.toList());
         return tickets;
 
     }
